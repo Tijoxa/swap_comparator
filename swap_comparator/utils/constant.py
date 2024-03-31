f919be30bb604aab785379f35d696280fcffb415
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2024-03-31 19:12:36 +0200

    fix constant

--- swap_comparator/utils/constant.py
+++ swap_comparator/utils/constant.py
@@ -13,14 +13,23 @@ class MainnetAddress:
     decimals: int
 
 
-MAINNET_ADDRESS = {
-    "WETH": MainnetAddress("0xC02AAA39B223FE8D0A0E5C4F27EAD9083C756CC2", 18),
-    "WBTC": MainnetAddress("0x2260FAC5E5542A773AA44FBCFEDF7C193BC2C599", 8),
+ARBISCAN_MAINNET_ADDRESS = {
+    "WETH": MainnetAddress("0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", 18),
+    "WBTC": MainnetAddress("0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f", 8),
+    "USDC": MainnetAddress("0xaf88d065e77c8cC2239327C5EDb3A432268e5831", 6),
+    "USDT": MainnetAddress("0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9", 6),
+    "ARB": MainnetAddress("0x912CE59144191C1204E64559FE8253a0e49E6548", 18),
+    "WSTETH": MainnetAddress("0x0fBcbaEA96Ce0cF7Ee00A8c19c3ab6f5Dc8E1921", 18),
+}
+
+ETHERSCAN_MAINNET_ADDRESS = {
+    "WETH": MainnetAddress("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", 18),
+    "WBTC": MainnetAddress("0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599", 8),
     "USDC": MainnetAddress("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", 6),
     "USDT": MainnetAddress("0xdAC17F958D2ee523a2206206994597C13D831ec7", 6),
+    "ARB": MainnetAddress("0xB50721BCf8d664c30412Cfbc6cf7a15145234ad1", 18),
+    "WSTETH": MainnetAddress("0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0", 18),
     "MKR": MainnetAddress("0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2", 18),
-    "ARB": MainnetAddress("0x7ceb23fd6bc0add59e62ac25578270cff1b9f619", 18),
-    "WSTETH": MainnetAddress("0x7f39C581F595B53c5CeB19Ee6B41CEfeE6F570A3", 18),
 }
 
 
@@ -28,3 +37,15 @@ class AmountCategory:
     StableCoin = [100, 1000, 10000, 100000, 1000000]
     WETH = [0.01, 0.1, 1, 10, 100, 1000, 10000]
     WBTC = [0.01, 0.1, 1, 10, 100, 1000, 10000]
+
+
+@dataclass
+class Chain_ID:
+    ID: int
+
+
+CHAIN_ID = {
+    "ETH": Chain_ID(1),
+    "BSC": Chain_ID(56),
+    "ARBITRUM": Chain_ID(42161),
+}
--- tests/utils/test_utils_1inch.py
+++ tests/utils/test_utils_1inch.py
@@ -1,10 +1,10 @@
 from swap_comparator.utils.utils_1inch import get_1inch_price
-from swap_comparator.utils.constant import MAINNET_ADDRESS
+from swap_comparator.utils.constant import ARBISCAN_MAINNET_ADDRESS
 
 
 def test_get_1inch_price():
     get_1inch_price(
-        token_in=MAINNET_ADDRESS["WETH"],
-        token_out=MAINNET_ADDRESS["WBTC"],
+        token_in=ARBISCAN_MAINNET_ADDRESS["WETH"],
+        token_out=ARBISCAN_MAINNET_ADDRESS["WBTC"],
         amount_in=1e18,
     )
