9f6d8e7f0c359be0ead936b9c45f39c1ffc28ef1
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2024-04-02 11:19:49 +0200

    prettier 1inch tests

--- tests/utils/test_utils_1inch.py
+++ tests/utils/test_utils_1inch.py
@@ -5,17 +5,21 @@ from swap_comparator.utils.constant import ID1inch, ChainList, ArbiscanMainnet
 
 def test_1inch_API():
     method = "get"
-    apiUrl = f"https://api.1inch.dev/swap/v6.0/{ChainList.ARBITRUM.ID}/quote"
+    api_url = f"https://api.1inch.dev/swap/v6.0/{ChainList.ARBITRUM.ID}/quote"
     requestOptions = {
         "headers": {"Authorization": f"Bearer {ID1inch}"},
         "body": {},
-        "params": {"src": ArbiscanMainnet.USDC.token_address, "dst": ArbiscanMainnet.USDT.token_address, "amount": 100},
+        "params": {
+            "src": ArbiscanMainnet.USDC.token_address,
+            "dst": ArbiscanMainnet.USDT.token_address,
+            "amount": 100,
+        },
     }
 
     headers = requestOptions.get("headers", {})
     body = requestOptions.get("body", {})
     params = requestOptions.get("params", {})
 
-    response = requests.get(apiUrl, headers=headers, params=params)
+    response = requests.get(api_url, headers=headers, params=params)
 
     assert response.status_code == 200, f"Error in Response: {response.text}"
